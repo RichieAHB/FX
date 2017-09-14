cf129d6100284157b29a21e1a0a6568549fccbb3
Richard Beddington <richard.beddington@gmail.com>
2017-09-14 09:59:57 +0100

    Add in real service

--- src/services/FXAPI.js
+++ src/services/FXAPI.js
@@ -15,11 +15,9 @@ const apiURI = (path, opts = {}) => {
 const fetchURI = (path, opts = {}) =>
   fetch(apiURI(path, opts)).then(response => response.json());
 
-// const latest = base => fetchURI('/latest.json', /* {
-//   base, // not available with free account
-// } */);
-
-const latest = () => fetch('/latest.json').then(response => response.json());
+const latest = base => fetchURI('/latest.json', /* {
+  base, // not available with free account
+} */);
 
 export {
   latest,
